d82606a0e4bd882b83c0ec569442aabf3c85a9e5
Andrew Piksaev <pikcaev@gmail.com>
2020-08-20 20:37:03 +0300

    Added Sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // importing styles
 import "./scss/main.scss";
 
+// importing Sentry
+import * as Sentry from "@sentry/browser";
+Sentry.init({ dsn: "https://20d50f6561574b8691a0f2c8a398d857@o412971.ingest.sentry.io/5398669" });
+
 // importing jquery-mousewheel plugin
 require('jquery-mousewheel');
 
